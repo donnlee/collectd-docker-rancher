2e07d898574af7eec4c963a0e75df4bbafdf1090
Donn Lee <git@pluza.com>
2016-06-20 19:02:54 -0700

    Overwrite env var for collectd hostname string.  Then run.sh has correct hostname to use in collectd.conf

--- get_hostname.sh
+++ get_hostname.sh
@@ -6,7 +6,10 @@ COLLECTD_HOSTNAME=$(curl -s http://rancher-metadata/2015-12-19/self/host/hostnam
 
 if [[ "$COLLECTD_HOSTNAME" ]]
 then
-  /usr/bin/perl -p -i -e "s/Hostname \".+\"/Hostname \"${COLLECTD_HOSTNAME}\"/g" /etc/collectd/collectd.conf
+  # Overwrite the env var used by run.sh to configure collectd.conf
+  HOST_NAME="$COLLECTD_HOSTNAME"
+  # Can't do regex substitution because run.sh hasn't processed .conf files yet.
+  #/usr/bin/perl -p -i -e "s/Hostname \".+\"/Hostname \"${COLLECTD_HOSTNAME}\"/g" /etc/collectd/collectd.conf
 else
   echo "Failed to get host's hostname from rancher-metadata API." > debug
 fi
